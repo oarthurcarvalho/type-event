667ebfa5715fef752c6a6ba74c0ac68cadd6b871
Arthur Carvalho <arthur.reis@live.com>
2023-04-27 13:05:47 -0300

    Criação da página de gerenciar evento

--- eventos/urls.py
+++ eventos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('novo_evento/', views.novo_evento, name='novo_evento'),
+    path('gerenciar_evento/', views.gerenciar_evento, name='gerenciar_evento')
 
 ]
--- eventos/views.py
+++ eventos/views.py
@@ -43,3 +43,8 @@ def novo_evento(request):
         messages.add_message(request, constants.SUCCESS,
                              'Evento cadastrado com sucesso')
         return redirect(reverse('novo_evento'))
+
+
+def gerenciar_evento(request):
+    if request.method == 'GET':
+        return render(request, 'gerenciar_evento.html')
